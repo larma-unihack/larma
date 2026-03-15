14777206446b27641e1e9f9cfd21b3a7d49b40b8
Risu <79110363+risu729@users.noreply.github.com>
2026-03-15 11:30:05 +1100

    fix build error

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,18 +1,23 @@
-import * as admin from 'firebase-admin';
+import { applicationDefault, cert, getApps, initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
-if (!admin.apps.length) {
-    try {
-        const serviceAccountStr = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
-        if (!serviceAccountStr) {
-            console.warn("FIREBASE_SERVICE_ACCOUNT_KEY is not set. Admin SDK may fail to authenticate.");
-        }
-
-        admin.initializeApp({
-            credential: serviceAccountStr ? admin.credential.cert(JSON.parse(serviceAccountStr)) : admin.credential.applicationDefault()
-        });
-    } catch (error) {
-        console.error("Firebase Admin initialization error", error);
+if (!getApps().length) {
+  try {
+    const serviceAccountStr = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+    if (!serviceAccountStr) {
+      console.warn(
+        "FIREBASE_SERVICE_ACCOUNT_KEY is not set. Admin SDK may fail to authenticate."
+      );
     }
+
+    initializeApp({
+      credential: serviceAccountStr
+        ? cert(JSON.parse(serviceAccountStr))
+        : applicationDefault(),
+    });
+  } catch (error) {
+    console.error("Firebase Admin initialization error", error);
+  }
 }
 
-export const adminDb = admin.firestore();
+export const adminDb = getFirestore();
